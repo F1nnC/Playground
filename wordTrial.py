e363902031a9d94af8fcd4dfd741f77b5616663c
EdwinKuttappi <edwinab0007@gmail.com>
2023-04-20 14:06:28 -0700

    add hints

--- wordTrial.py
+++ wordTrial.py
@@ -9,21 +9,28 @@ scrambled_word = "".join(random.sample(word, len(word)))
 print(f"Unscramble the following word: {scrambled_word}")
 
 hints = 1
-
 guesses = 1
 guess = input("What's the unscrambled word? ").lower()
 
-while guess != word and guesses < 3:
+# != means not equal to
+while guess != word and guesses == 1:
     print("Sorry, that's not the word. Try again!")
+    guess = input("What's the unscrambled word? ").lower()
+    guesses += 1 
+
+while guess != word and guesses == 2:
+    print("Sorry, that's not the word. Try again!")
+    print(f"Hint 1: The first letter of the word is '{word[0]}'")
     guess = input("What's the unscrambled word? ").lower()
     guesses += 1
 
-if guesses == 2:
-    print(f"Hint 1: The first letter of the word is '{word[0]}'")
-if guesses == 1:
-    print(f"Hint 2: The second letter of the word is '{word[1]}")
+while guess != word and guesses == 3:
+    print("Sorry, that's not the word. Try again!")
+    print(f"Hint 2: The second letter of the word is '{word[1]}'")
+    guess = input("What's the unscrambled word? ").lower()
+    guesses += 1
 
 if guess == word:
     print("Congratulations, you unscrambled the word!")
 else:
-    print(f"Sorry, you didn't unscramble the word. The word was {word}.")
+    print(f"All 3 Guesses have been used, you didn't unscramble the word, the word was {word}")
